6c6ec5aae0a92c678339af28bd9ddfb5a347a484
mxdillon <marowidi@gmail.com>
2020-01-02 18:19:51 +0000

    fix: got working on localhost but automatically creates get request

--- src/server.py
+++ src/server.py
@@ -20,14 +20,12 @@ def index_route():
 
         text_input = request.args.get('text_input')
         if text_input is None:
-            text_input = " "
+            text_input = ' '
 
-        else:
-            generated_text = call_generator(start_phrase=text_input,
-                                            weights_path='./model/1_2la512-256emb512lr003/ckpt_50',
-                                            string_length=500)
+        generated_text = call_generator(start_phrase=text_input,
+                                        weights_path='./model/1_2la512-256emb512lr003/ckpt_50',
+                                        string_length=500)
 
-        return render_template('index.html', text_input=text_input,
-                               result=generated_text)
+        return render_template('index.html', text_input=text_input, result=generated_text)
 
     return render_template('index.html')
