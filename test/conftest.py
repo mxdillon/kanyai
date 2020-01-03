4ada60c062ccf7f2d3dedbb17e919335eb62aa05
j-penson <jpenson24@gmail.com>
2020-01-03 22:17:25 +0000

    fix: mock gcloud auth credentials

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,16 @@
 import pytest
-from app import create_app
+
+
+@pytest.fixture(autouse=True)
+def env_setup(monkeypatch):
+    monkeypatch.setenv('GOOGLE_APPLICATION_CREDENTIALS',
+                       './test/mock-secrets/sample-creds.json')
 
 
 @pytest.fixture
 def client():
     """Flask test client."""
+    from app import create_app
     app = create_app()
     client = app.test_client()
     return client
