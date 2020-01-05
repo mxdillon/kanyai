8205f371204ea81f276d326e226c7dd255c3100d
mxdillon <marowidi@gmail.com>
2020-01-05 16:54:29 +0000

    feat: add additional swear words to standard list

---
+++ src/config/profanity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# coding=utf-8
+"""Custom profane terms that are not included in the better-profanity python package
+:usage:
+    List is called in app.py
+:authors
+    05/01/20
+"""
+
+custom_badwords = ['bitchs',
+                   'bitchez',
+                   'ho',
+                   'hoe',
+                   'hos',
+                   'hoes',
+                   'niggas',
+                   'niggers',
+                   ]
