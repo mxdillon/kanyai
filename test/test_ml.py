470075d02941c77d5fe8e0065f68d2ca92b70832
mxdillon <marowidi@gmail.com>
2020-01-07 09:15:51 +0000

    feat: adding fixtures

--- test/test_ml.py
+++ test/test_ml.py
@@ -9,9 +9,11 @@
 import pytest
 from src.ml.generate_lyrics import GenerateLyrics
 
+
 @pytest.fixture(scope='session')
 def generator_class():
     return GenerateLyrics(embedding_dim=512)
+
 
 @pytest.fixture(scope='session')
 def load_maps(generator_class):
@@ -19,3 +21,8 @@ def load_maps(generator_class):
         character_map_load_path='./model/2_noheaders/character_index_map.json',
         index_map_load_path='./model/2_noheaders/index_character_map.npy')
 
+
+@pytest.fixture(scope='session')
+def rebuild_model(load_maps):
+    load_maps.rebuild_model(batch_size=1,
+                            weights_path='./model/2_noheaders/ckpt_60')
