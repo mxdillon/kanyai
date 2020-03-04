c76b1dd84905f378f8761f4e20abbb3c0bb423a5
j-penson <jpenson24@gmail.com>
2020-03-04 10:33:18 +0000

    feat: strip out google logging, prep for azure deployment

--- test/conftest.py
+++ test/conftest.py
@@ -8,16 +8,7 @@
 """
 
 import pytest
-import logging
-from google.cloud.logging.client import Client
 from app.ml.generate_lyrics import GenerateLyrics
-
-
-@pytest.fixture(autouse=True)
-def env_setup(monkeypatch):
-    """Mock the credentials environment variable for when the Google logging client is initialised."""
-    monkeypatch.setenv('GOOGLE_APPLICATION_CREDENTIALS',
-                       './test/mock-data/sample-creds.json')
 
 
 @pytest.fixture
@@ -26,14 +17,6 @@ def client():
     from main import create_app
     app = create_app()
     client = app.test_client()
-
-    # Remove the Google Cloud logging client for local tests
-    test_logger = logging.getLogger()
-    for handler in test_logger.handlers:
-        if hasattr(handler, 'client'):
-            if isinstance(handler.client, Client):
-                test_logger.removeHandler(handler)
-
     return client
 
 
