792021e898deb1fbe4717d4a55e1a404ad7a6228
mxdillon <marowidi@gmail.com>
2020-03-28 19:46:59 +0000

    feat: switch to tf 1.15.0 for gpt2-simple to run feat: remove old style of character level model

--- test/unit/test_generate_lyrics.py
+++ test/unit/test_generate_lyrics.py
@@ -6,15 +6,3 @@
 :authors
     MD at 03/01/20
 """
-
-
-def test_character_map_loaded(load_maps):
-    assert load_maps.char_to_ind_map is not None
-
-
-def test_index_map_loaded(load_maps):
-    assert load_maps.ind_to_char_map is not None
-
-
-def test_model_rebuilt(rebuild_model):
-    assert rebuild_model is not None
